7d238fbd235a9287bad822fda6a0cb9f9774b595
vibha <ucvibha@gmail.com>
2022-08-02 20:27:31 +0530

    added helloWorld.java file

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld{
+	public static void main(String args[]){
+		while(true){
+			System.out.println("Hello World");
+			try{
+				Thread.sleep(60000);
+			}
+			catch(Exception ex){
+				System.out.println(ex);
+			}
+		}
+	}
+}
